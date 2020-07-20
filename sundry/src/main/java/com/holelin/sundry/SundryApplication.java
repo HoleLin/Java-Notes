a27aec2c7b8a0e326351dd6cd9458a9c456736c0
HoleLin <HoleLin@163.com>
2020-07-20 09:47:10 +0800

    添加杂项模块

---
+++ sundry/src/main/java/com/holelin/sundry/SundryApplication.java
@@ -0,0 +1,13 @@
+package com.holelin.sundry;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SundryApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SundryApplication.class, args);
+    }
+
+}
