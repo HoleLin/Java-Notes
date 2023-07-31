2bf8f20a46d62a14385cb2fe731a2eac1bd73b79
HoleLin <HoleLin@163.com>
2023-07-31 09:50:29 +0800

    add some files

--- spring-demo/src/test/java/cn/holelin/springdemo/SpringDemoApplicationTests.java
+++ spring-demo/src/test/java/cn/holelin/springdemo/SpringDemoApplicationTests.java
@@ -2,10 +2,8 @@ package cn.holelin.springdemo;
 
 import cn.holelin.springdemo.core.customElement.bean.User;
 import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
-import org.springframework.transaction.annotation.Transactional;
 
 class SpringDemoApplicationTests {
 
